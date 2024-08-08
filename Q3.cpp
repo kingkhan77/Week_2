e4874ec86d7c2a0f8b2226fe9f231e1092e3cf44
Shahzad Ullah Khan <123242451+kingkhan77@users.noreply.github.com>
2024-08-08 15:51:28 +0530

    Create Q3.cpp
    Print frequency of each word in a sentence

---
+++ Q3.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void printFrequency(string str)
+{
+    map<string, int> M;
+    
+    string word = "";
+ 
+    for (int i = 0; i < str.size(); i++) {
+        if (str[i] == ' ') {
+            if (M.find(word) == M.end()) {
+                M.insert(make_pair(word, 1));
+                word = "";
+            }
+ 
+            // update the frequency
+            else {
+                M[word]++;
+                word = "";
+            }
+        }
+ 
+        else
+            word += str[i];
+    }
+ 
+       if (M.find(word) == M.end())
+        M.insert(make_pair(word, 1));
+ 
+        else
+        M[word]++;
+        
+    cout<<"Word \t    Frequency\n\n";
+    
+    for (auto& it : M) {
+        cout << it.first << "\t -> \t"
+             << it.second
+             << endl;
+    }
+}
+ 
+
+
+int main()
+{
+    string str = "Geeks For Geeks";
+ 
+    printFrequency(str);
+    return 0;
+}
